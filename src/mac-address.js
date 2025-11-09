1fe025f8db23ef319973c21d6b15db8990bf8232
Nastassia Romanova <romanova7410020@gmail.com>
2025-11-09 13:27:35 +0300

    fix: add solution DepthCalculator task

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,9 @@ const { NotImplementedError } = require('../lib');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  const mac48 = /^([0-9A-F]{2}-){5}[0-9A-F]{2}$/;
+  return mac48.test(n);
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) {
+      return 0};
+    if (arr.length === 0) {
+      return 1;
+    }
+    let depth = 0;
+    for (const el of arr) {
+      depth = Math.max (depth, this.calculateDepth(el));
+    }
+    return depth + 1;
   }
 }
 
